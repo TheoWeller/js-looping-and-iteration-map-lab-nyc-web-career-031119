13fff7e382ef9e6608931131ece87b9fbd50a154
Theo Weller <weller.theodor@gmail.com>
2019-04-23 01:46:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ function lowerCaseDrivers(drivers) {
   let newArray = [];
   drivers.map(driver => newArray.push(driver.toLowerCase()));
   return newArray;
+};
+
+function nameToAttributes(drivers) {
+  let newArray = [];
+  let counter = 0;
+  drivers.map(driver => newArray.push(driver.split(" ")))
+  newArray.map(driver => {firstName: newArray[counter++][0], lastName: newArray[counter++][1]});
+  return newArray;
 }
