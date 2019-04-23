1bbdd319857dfe95600dfe801ec631806841a503
Theo Weller <weller.theodor@gmail.com>
2019-04-23 01:36:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
   let newArray = [];
-  drivers.map(driver => newArray.push(driver.toLowerCase()))
+  drivers.map(driver => newArray.push(driver.toLowerCase()));
+  return newArray;
 }
